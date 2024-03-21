b31430c46d07546126b81605c75d8021dbb3ab02
gks-96 <gurkiratguliani@gmail.com>
2024-03-21 05:38:02 -0700

    section-divider completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import About from '@/components/about'
 import Intro from '@/components/intro'
+import SectionDivider from '@/components/section-divider'
 
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-cdnter *:px-4">
+    <main className="flex flex-col items-cdnter px-4">
       <Intro/>
+      <SectionDivider/>
+      {/* <About/> */}
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -52,7 +52,7 @@ export default function Intro() {
           </motion.span>
           </div>
       </div>
-      <motion.p className="mb-10 mt-4 px-4 text-2xl
+      <motion.h1 className="mb-10 mt-4 px-4 text-2xl
       font-medium !leading-[1.5] sm:text-4xl"
       initial={{opacity:0,y:100}}
       animate={{opacity:1,y:0}}
@@ -60,7 +60,7 @@ export default function Intro() {
       <span className="font-bold">Hello, I'm Gurkirat.</span> I'm a {" "}
         <span className="font-bold">full-stack developer</span> with {" "}
         <span className="font-bold">3 years</span> of experience. 
-      </motion.p>
+      </motion.h1>
 
       <motion.div className="flex flex-col sm:flex-row
       items-center justify-center gap-2 px-4 
