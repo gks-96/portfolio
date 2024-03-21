270f8c8820b8f0774cb3c9728c9117b93a940881
gks-96 <gurkiratguliani@gmail.com>
2024-03-21 16:13:48 -0700

    project section completed

--- components/intro.tsx
+++ components/intro.tsx
@@ -15,8 +15,8 @@ export default function Intro() {
   // const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
-    <section className="mb-28 max-w-[50rem] text-center
-     sm:mb-0">
+    <section id="home" className="mb-28 max-w-[50rem] text-center
+     sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
         <motion.div
---
+++ components/projects.tsx
@@ -0,0 +1,27 @@
+
+import React  from 'react'
+import SectionHeading from './section-heading'
+import { projectsData} from "@/lib/data";
+import Image from "next/image";
+import {useScroll} from "framer-motion"
+import Project from "@/components/project"
+
+export default function Projects() {
+  return (
+   <section id="projects" className="scroll-mt-28" >
+    <SectionHeading> My Projects</SectionHeading>
+    <div>
+        {
+        projectsData.map((project,index)=>(
+            <React.Fragment key={index}>
+            <Project {...project} />
+            </ React.Fragment>
+        )) 
+        }
+    </div>
+    <li>
+        
+    </li>
+    </section>
+  )
+}
