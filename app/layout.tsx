8c06cb84ff5bf4d7720ae88f6abb8a1344a3b1e8
gks-96 <gurkiratguliani@gmail.com>
2024-03-22 04:08:56 -0700

    footer completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Header from "@/components/header";
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from "react-hot-toast";
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,6 +37,7 @@ export default function RootLayout({
           {children}
 
           <Toaster position="top-right"/>
+          <Footer/>
         </ActiveSectionContextProvider>
         
         </body>
